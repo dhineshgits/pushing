a23c73ac5f4966759b79d752f85fa5a19700009a
dhineshgits <dhineshmr611@gamil.com>
2023-02-07 19:56:54 +0530

    plz go

---
+++ Sum_of_gived_no.java
@@ -0,0 +1,14 @@
+class Sum_of_gived_no 
+{
+	public static void main(String[] args) 
+	{
+		int sum = 0;
+
+		for (int i=1;i<=20;i++)
+		{
+           sum = sum + i;
+           
+		}
+		System.out.println("The sum of given number is " + sum);
+	}
+}
