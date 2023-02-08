bc46402878087e1371388f383ed0aa545108222b
dhineshgits <dhineshmr611@gamil.com>
2023-02-08 18:49:06 +0530

    wednesday

---
+++ Parellelogram.java
@@ -0,0 +1,18 @@
+class Parellelogram 
+{
+	 int Parellelogram(int b ,int h)
+	{
+	  int area =b*h;
+	  return area;
+	  
+	}
+
+
+
+
+	public static void main(String[] args) 
+	{
+		int area = new Parellelogram().Parellelogram(4,6);
+		System.out.println("The area of Parellelogram is " + area);
+	}
+}
